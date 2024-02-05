07b09941dcb424a9a01c7f3b9d1e138b80a6abca
MISTUser <MISTUser@lab8L4JUY>
2024-02-05 18:37:20 -0500

    Add project files.

---
+++ Assignment2.1Martin/Pages/SearchResults.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Assignment2._1Martin.Pages
+{
+    public class Index1Model : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
